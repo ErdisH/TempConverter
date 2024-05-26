dd2d7dd3d93750d30a9530305758651430b6c690
ErdisH <erdis.halluni.1@ens.etsmtl.ca>
2024-05-25 21:33:35 -0400

    Absolute insanity

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+const runButton = document.getElementById("okButton");
+const input = document.getElementById("numInput");
+const label = document.getElementById("numLabel");
+const resultLabel= document.getElementById("resultLabel");
+const resetButton = document.getElementById("resetButton");
 let value=0;
-document.getElementById("minusButton").onclick = function(){
-    if(value-1>=0) value--;
-    document.getElementById("numberLabel").textContent = value; 
+let tries=0, wins=0;
+
+runButton.onclick = function(){
+    value=Math.floor((Math.random()*6)+1)
+    label.textContent = value;
+    tries++;
+    let enteredValue = input.value;
+    if(Number(enteredValue)===value)
+    {
+        wins++;
+        resultLabel.textContent = `You win, number of wins: ${wins}, total tries: ${tries}`;
+    }
+    else
+    {
+        resultLabel.textContent = `Wrong guess, number of wins: ${wins}, total tries: ${tries}`;
+    }
 };
-document.getElementById("addButton").onclick = function(){
-    value++;
-    document.getElementById("numberLabel").textContent = value; 
-};
-document.getElementById("resetButton").onclick = function(){
-    value=0;
-    document.getElementById("numberLabel").textContent = value; 
+
+resetButton.onclick = function(){
+    wins=0;
+    tries=0;
+    label.textContent = 0;
+    resultLabel.textContent = "Game has been reset at 0 tries 0 wins. GLHF!"
 };
